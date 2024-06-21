34c54a44eb80c0a43835abc71334072048b18db7
bbqbop <dierk.peters@gmail.com>
2024-06-21 19:00:08 -0400

    add netlify

--- frontend/src/components/GameOver.jsx
+++
@@ -1,8 +0,0 @@
-export default function GameOver(){
-    return(
-        <div className="gameOver">
-            You've found all the characters! 
-            <a href="/">Home</a>
-        </div>
-    )
-}
